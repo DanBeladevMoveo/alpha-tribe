ff1b183b2dae5eb40926cd80d58e6cab62144a45
Dan Beladev <danb@moveo.co.il>
2021-04-04 17:53:52 +0300

    add save abillity

--- src/components/alpha-time/alpha-time.js
+++ src/components/alpha-time/alpha-time.js
@@ -4,9 +4,11 @@ import firebase from "firebase";
 import usersAlpha from "../../helpers/alpha";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import SaveIcon from '@material-ui/icons/Save';
 
 const AlphaTime = () => {
   const [couples, setCouples] = useState([]);
+  const [id, setID] = useState('');
   const [startDate, setStartDate] = useState(new Date());
   const db = firebase.firestore();
 
@@ -19,7 +21,8 @@ const AlphaTime = () => {
          const isEmpty = res.empty;
           if (!isEmpty) {
             const current = res.docs[res.docs.length - 1];
-             console.log(current.data());
+            setID(current.id);
+            //  console.log('current:', current, ' currentdata: ',current.data());
             const { alphaCouples } = current.data();
              setCouples(alphaCouples);
          }
@@ -62,6 +65,39 @@ const AlphaTime = () => {
     return arr;
   };
 
+  const saveWeek = (couple, i) => {
+
+    if(isSaveIconDisabled(couple,i)) return
+
+    const firstValue = document.getElementById('first' + i).value;
+    const secondValue = document.getElementById('second' + i).value;
+    const dateValue = document.getElementById('date' + i).value;
+
+    const payload = {
+      first: firstValue, second: secondValue, date: dateValue 
+    }
+    const newArray = [...couples];
+    newArray[i] = payload;
+    db.collection("alpha-time")
+    .doc(id)
+    .set({alphaCouples: newArray})
+    .then((res) => {
+       console.log("update couple successfuly", res);
+        setCouples(newArray);
+        alert('updated successfully');
+      })
+    .catch((err) => {
+        console.error("failed get alpha time");
+      });
+  };
+
+  const isSaveIconDisabled = (couple, i) => {
+    const firstValue = document.getElementById('first' + i)?.value;
+    const secondValue = document.getElementById('second' + i)?.value;
+    const dateValue = document.getElementById('date' + i)?.value;
+    return (couple.first === firstValue && couple.second === secondValue && couple.date === dateValue)
+  }
+
   const Shuffle = () => {
     const alphaCouples = [];
     let alpha = [...usersAlpha];
@@ -79,14 +115,11 @@ const AlphaTime = () => {
         second: secondUser?.name || null,
         date: formattedDate,
       };
-      // let string = couple.second
-      //   ? `${couple.first} - ${couple.second} - ${couple.date}`
-      //   : `${couple.first} - ${couple.date}`;
       alphaCouples.push(couple);
-      setCouples(alphaCouples);
       date.setDate(date.getDate() + 7);
       formattedDate = formatDate(date);
     }
+    setCouples(alphaCouples);
     saveCouples(alphaCouples);
   };
 
@@ -108,9 +141,10 @@ return (
         </div>
         <div className="result-list">
           <ul className="result-list">
-            {couples && couples.map((couple) => (
+            {couples && couples.map((couple,i) => (
               <li key={couple.first} className="member result-member">
-                <input className="member-input" defaultValue={couple.first}/><span className="operator">+</span><input className="member-input" defaultValue={couple.second}/> <span className="operator">=</span><input className="member-input" defaultValue={couple.date}/>
+                <input id={ `first${i}`} className="member-input" defaultValue={couple.first}/><span className="operator">+</span><input id={`second${i}`} className="member-input" defaultValue={couple.second}/> <span className="operator">=</span><input id={`date${i}`} className="member-input" defaultValue={couple.date}/>
+                <SaveIcon className="save" onClick = {() => saveWeek(couple,i)}></SaveIcon>
               </li>
             ))}
           </ul>
